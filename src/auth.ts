1fd78804c3aa034f44b2ac4e1a37c1d711086354
Jeffreyyvdb <jeffrey.2001.2312@gmail.com>
2024-11-24 00:54:30 +0100

    persist users in database

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
+import { db } from './lib/server/db';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	providers: [GitHub],
+	adapter: DrizzleAdapter(db),
 	trustHost: true
 });
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -8,7 +8,10 @@ import {
 	serial,
 	integer,
 	date,
-	decimal
+	decimal,
+	primaryKey,
+	boolean,
+	uniqueIndex
 } from 'drizzle-orm/pg-core';
 import { generateId } from 'ai';
 import { sql } from 'drizzle-orm';
@@ -64,7 +67,7 @@ export const insertResourceSchema = createSelectSchema(resources).extend({}).omi
 export type NewResourceParams = z.infer<typeof insertResourceSchema>;
 
 export const unicorns = pgTable('unicorns', {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
 	company: varchar('company', { length: 255 }).notNull(),
 	valuation: decimal('valuation', { precision: 10, scale: 1 }).notNull(),
 	date: timestamp('date').notNull(),
@@ -73,3 +76,80 @@ export const unicorns = pgTable('unicorns', {
 	industry: varchar('industry', { length: 255 }).notNull(),
 	investors: text('investors').notNull()
 });
+
+export const users = pgTable('user', {
+	id: text('id')
+		.primaryKey()
+		.$defaultFn(() => crypto.randomUUID()),
+	name: text('name'),
+	email: text('email').unique(),
+	emailVerified: timestamp('emailVerified', { mode: 'date' }),
+	image: text('image')
+});
+
+export const accounts = pgTable(
+	'account',
+	{
+		userId: text('userId')
+			.notNull()
+			.references(() => users.id, { onDelete: 'cascade' }),
+		type: text('type').notNull(),
+		provider: text('provider').notNull(),
+		providerAccountId: text('providerAccountId').notNull(),
+		refresh_token: text('refresh_token'),
+		access_token: text('access_token'),
+		expires_at: timestamp('expires_at', { mode: 'date' }),
+		token_type: text('token_type'),
+		scope: text('scope'),
+		id_token: text('id_token'),
+		session_state: text('session_state')
+	},
+	(account) => ({
+		compoundKey: primaryKey({
+			columns: [account.provider, account.providerAccountId]
+		})
+	})
+);
+
+export const sessions = pgTable('session', {
+	sessionToken: text('sessionToken').primaryKey(),
+	userId: text('userId')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
+	expires: timestamp('expires', { mode: 'date' }).notNull()
+});
+
+export const verificationTokens = pgTable(
+	'verificationToken',
+	{
+		identifier: text('identifier').notNull(),
+		token: text('token').notNull(),
+		expires: timestamp('expires', { mode: 'date' }).notNull()
+	},
+	(verificationToken) => ({
+		compositePk: primaryKey({
+			columns: [verificationToken.identifier, verificationToken.token]
+		})
+	})
+);
+
+export const authenticators = pgTable(
+	'authenticator',
+	{
+		credentialID: text('credentialID').notNull().unique(),
+		userId: text('userId')
+			.notNull()
+			.references(() => users.id, { onDelete: 'cascade' }),
+		providerAccountId: text('providerAccountId').notNull(),
+		credentialPublicKey: text('credentialPublicKey').notNull(),
+		counter: integer('counter').notNull(),
+		credentialDeviceType: text('credentialDeviceType').notNull(),
+		credentialBackedUp: boolean('credentialBackedUp').notNull(),
+		transports: text('transports')
+	},
+	(authenticator) => ({
+		compositePK: primaryKey({
+			columns: [authenticator.userId, authenticator.credentialID]
+		})
+	})
+);
